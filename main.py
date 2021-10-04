3d133e45d545dfcb8354f74ba7f43e93355aa6d1
EcaterinaCnt <catyellinida@yahoo.com>
2021-10-04 23:23:00 +0300

    doua probleme

--- main.py
+++ main.py
@@ -19,4 +19,28 @@ def test_get_leap_years():
     assert get_leap_years(2000, 2021) == [2000, 2004, 2008, 2012, 2016, 2020]
     assert get_leap_years(1990, 2001) == [1992, 1996, 2000]
 
+def is_antipalindrome(n) -> bool:
+    cifre_n = []
+    while n > 0:
+        cifre_n.append(n % 10)
+        n = n // 10
+
+    lenght=len(cifre_n)
+    '''
+    i=0
+    while i<=lenght//2:
+        if cifre_n[i] == cifre_n[lenght-1-i]:
+            return False
+        i=i+1
+'''
+    for i in range(0,lenght//2):
+          if cifre_n[i] == cifre_n[lenght-1-i]:
+              return False
+
+    return True
+def test_is_antipalindrome():
+    assert is_antipalindrome(2783) == True
+    assert is_antipalindrome(2773) == False
+
 test_get_leap_years()
+test_is_antipalindrome()
